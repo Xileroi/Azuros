340ce85dce613739ae48c4aa16dc82418ec4a161
Xileroi <xeleroy@gmail.com>
2023-09-27 06:16:00 +0200

    Adding some texts and plugins

--- src/pages/projects.page.tsx
+++ src/pages/projects.page.tsx
@@ -20,7 +20,29 @@ export function Page() {
       <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8 flex-grow">
         <div>
           <Delimit text='BSQ 42'/>
-          <p className="text-white">Test</p>
+          <div>
+      <div className="flex flex-col items-center">
+      <h1 className="text-2xl font-bold mb-4 py-5">Projet de l'école 42</h1>
+      <div className="flex">
+        <div className="w-1/2 mr-4">
+          <ul>
+            <li>Sur ce projet de l'école 42, plusieurs tâches doivent être accomplies.</li>
+            <li>Tout d'abord, il est essentiel d'afficher une carte composée de caractères représentant le vide, des caractères représentant des obstacles, et des caractères servant à afficher la solution à notre problème.</li>
+            <li>Le problème est simple : trouver l'espace le plus grand possible pour y construire un carré.</li>
+            <li>Nous effectuons donc plusieurs vérifications pour nous assurer que la carte fournie en paramètre est au bon format et contient les données nécessaires.</li>
+            <li>Nous vérifions également que le nombre de paramètres fournis lors du lancement du programme est correct.</li>
+          </ul>
+        </div>
+        <div className="w-1/2">
+          <ul>
+            <li>Ensuite, notre algorithme commence par rechercher s'il est possible de créer un carré à l'emplacement (0, 0) de la carte.</li>
+            <li>Ensuite, il stocke la taille et les coordonnées de ce carré, puis recherche un autre emplacement pour un carré, par exemple à la position (0, 1), et ainsi de suite jusqu'à parcourir toute la carte.</li>
+            <li>Enfin, il affiche la carte dans le terminal avec le carré le plus grand dessiné à l'intérieur.</li>
+          </ul>
+        </div>
+      </div>
+      </div>
+    </div>
         </div>
       </div>
     </Website>
